8a65ed8cc93b68cded693fc4fb8a101784685e61
student <student@labstudenti.finki.ukim.mk>
2019-04-04 10:38:07 +0200

    funciton implemented

--- program.cpp
+++ program.cpp
@@ -1,13 +1,16 @@
+
 #include <iostream>
 #include <cstring>
 using namespace std;
 
 void printMyName () {
 	//TODO
+	cout<<"Kostadin Bozinovski"<<endl;
 }
 
 void printMyIndex () {
 	//TODO
+	cout<<"153101"<<endl;
 }
 
 int main () {
